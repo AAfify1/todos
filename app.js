063821ac9368776a6a4d63fb132d79e0c07e57c6
Ahmed Afify <a.afify1995@gmail.com>
2020-04-09 02:06:28 +0200

    rename

--- app.js
+++ app.js
@@ -1,82 +1,91 @@
-var createError = require('http-errors');
-var express = require('express');
-var mongoose = require('mongoose');
-var Promise = require('bluebird');
+var createError = require("http-errors");
+var express = require("express");
+var mongoose = require("mongoose");
+var Promise = require("bluebird");
 Promise.promisifyAll(mongoose);
-var exphbs  = require('express-handlebars');
-var sassMiddleware = require('node-sass-middleware');
-var browserify = require('browserify-middleware')
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var exphbs = require("express-handlebars");
+var sassMiddleware = require("node-sass-middleware");
+var browserify = require("browserify-middleware");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var todosRouter = require('./routes/todos/index');
-var todosAPIRouter = require('./routes/todos/api');
-var instagramRouter = require('./routes/instagram')
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/instagram");
+var todosRouter = require("./routes/todos/index");
+var todosAPIRouter = require("./routes/todos/api");
 
 var app = express();
 
 // view engine setup
-app.engine('hbs', exphbs({extname: '.hbs', defaultLayout: 'layout'}));
-app.set('view engine','hbs');
+app.engine("hbs", exphbs({ extname: ".hbs", defaultLayout: "layout" }));
+app.set("view engine", "hbs");
 
-app.use ( 
+app.use(
   sassMiddleware({
-    src: __dirname + '/sass',
-    dest: __dirname + '/public',
-    debug: true
+    src: __dirname + "/sass",
+    dest: __dirname + "/public",
+    debug: true,
   })
 );
 
 browserify.settings({
-  transform: ['hbsfy']
+  transform: ["hbsfy"],
 });
-app.get('/javascripts/bundle.js',browserify('./client/script.js'));
+app.get("/javascripts/bundle.js", browserify("./client/script.js"));
 
-var dbConnectionString = process.env.MONGODB_URI || 'mongodb://localhost';
-mongoose.connect(dbConnectionString + '/todos');
+var dbConnectionString = process.env.MONGODB_URI || "mongodb://localhost";
+mongoose.connect(dbConnectionString + "/todos");
 
-if (app.get('env') == 'development') {
-  var browserSync = require('browser-sync');
+if (app.get("env") == "development") {
+  var browserSync = require("browser-sync");
   var config = {
-    files: ["public/**/*.{js,css}", "client/*.js", "sass/**/*.scss", "views/**/*.hbs"],
-    logLevel: 'debug',
+    files: [
+      "public/**/*.{js,css}",
+      "client/*.js",
+      "sass/**/*.scss",
+      "views/**/*.hbs",
+    ],
+    logLevel: "debug",
     logSnippet: false,
     reloadDelay: 3000,
-    reloadOnRestart: true
+    reloadOnRestart: true,
   };
   var bs = browserSync(config);
-  app.use(require('connect-browser-sync')(bs));
+  app.use(require("connect-browser-sync")(bs));
 }
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/fonts', express.static(path.join(__dirname, 'node_modules/bootstrap-sass/assets/fonts')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  "/fonts",
+  express.static(
+    path.join(__dirname, "node_modules/bootstrap-sass/assets/fonts")
+  )
+);
 
-app.use('/', indexRouter);
-app.use('/instagram', usersRouter);
-app.use('/todos',todosRouter);
-app.use('/api/todos',todosAPIRouter);
+app.use("/", indexRouter);
+app.use("/instagram", usersRouter);
+app.use("/todos", todosRouter);
+app.use("/api/todos", todosAPIRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
